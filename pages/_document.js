040271d31f6cc2ec7146babcfd39ed7fb65efd07
giacomomarino <giacomo_marino@brown.edu>
2023-05-23 09:56:34 -0400

    add GA

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XJ97HZ0JP4"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XJ97HZ0JP4');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
