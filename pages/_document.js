f1436249d134d542c9e2d08cd9262715aa5451ff
giacomomarino <giacomo_marino@brown.edu>
2023-05-30 10:04:03 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,22 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XJ97HZ0JP4"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+        <Script 
+          src="https://www.googletagmanager.com/gtag/js?id=G-XJ97HZ0JP4" 
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-XJ97HZ0JP4');
-            `,
-          }}
-        />
+            `}
+        </Script>
       </Head>
       <body>
         <Main />
