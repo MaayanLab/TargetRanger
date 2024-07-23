be5c3c44bfae6a65525c4bcd31bc51bab957c042
giacomomarino <77803629+giacomomarino@users.noreply.github.com>
2024-07-23 10:13:15 -0400

    Update targetResultsTable.js

--- components/targetResultsTable.js
+++ components/targetResultsTable.js
@@ -25,242 +25,50 @@ export default function TargetResultTable(props) {
 
   if (transcript_level) {
     columns = [
-      { field: "transcript", headerName: "Target", minWidth: 200, flex: 1 },
-      { field: "gene", headerName: "gene symbol", minWidth: 100, flex: 1 },
+      { field: "transcript", headerName: "Target", minWidth: 150, flex: 1 },
+      { field: "gene", headerName: "Gene Symbol", minWidth: 100, flex: 1 },
       {
         field: "t",
-        headerName: "t statistic",
+        headerName: "t stat",
         type: "number",
         flex: 1,
-        minWidth: 150,
+        minWidth: 70,
       },
       {
         field: "p",
         headerName: "P-value",
         type: "number",
         flex: 1,
-        minWidth: 200,
+        minWidth: 100,
       },
       {
         field: "adj_p",
         headerName: "Adj. P-value",
         type: "number",
         flex: 1,
-        minWidth: 200,
+        minWidth: 100,
       },
       {
         field: "log2fc",
-        headerName: "log2 Fold Change",
+        headerName: "log2FC",
         type: "number",
         flex: 1,
-        minWidth: 160,
+        minWidth: 70,
       },
       {
         field: "membrane",
         headerName: "Membrane Protien",
         type: "number",
         flex: 1,
-        minWidth: 150,
+        minWidth: 130,
       },
       {
         field: "secreted",
         headerName: "Secreted Protien",
         type: "number",
         flex: 1,
-        minWidth: 150,
-      },
-      {
-        field: "antibody",
-        headerName: "Antibody",
-        sortable: false,
-        flex: 1,
-        minWidth: 80,
-        renderCell: (params) => {
-          const onClickAntibodypedia = (e) => {
-            e.stopPropagation(); // don't select this row after clicking
-            window.open(
-              `https://www.antibodypedia.com/explore/${params.row.gene}`, "_blank", "noreferrer"
-            )
-          }
-          return (
-            <div
-              className={styles.horizontalFlexbox}
-              style={{ gap: "0px", padding: "0px" }}
-            >
-              <Tooltip title="Open in antibodypedia">
-                <Button onClick={onClickAntibodypedia}>
-                  <img
-                    style={{
-                      width: "20px",
-                      display: "flex",
-                      flexDirection: "row",
-                      gap: "0px",
-                      padding: "0px",
-                    }}
-                    src={
-                      runtimeConfig.NEXT_PUBLIC_ENTRYPOINT +
-                      "/images/antibodypedia.png"
-                    }
-                    alt="antibodypedia Logo"
-                  />
-                </Button>
-              </Tooltip>
-              </div>)
-        }
-      },
-      {
-        field: "action",
-        headerName: "Links",
-        sortable: false,
-        flex: 1,
-        minWidth: 320,
-        renderCell: (params) => {
-          const onClickENSEMBL = (e) => {
-            e.stopPropagation(); // don't select this row after clicking
-            window.open(
-              `https://useast.ensembl.org/Homo_sapiens/Transcript/Summary?t=${params.row.transcript}`,
-              "_blank",
-              "noreferrer"
-            );
-          };
-          const onClickARCHS4 = (e) => {
-            e.stopPropagation(); // don't select this row after clicking
-            window.open(
-              `https://maayanlab.cloud/archs4/gene/${params.row.gene}`,
-              "_blank",
-              "noreferrer"
-            );
-          };
-          const onClickHARMONIZOME = (e) => {
-            e.stopPropagation();
-
-            window.open(
-              `https://maayanlab.cloud/Harmonizome/gene/${params.row.gene}`
-            );
-          };
-          const onClickGDLPA = (e) => {
-            e.stopPropagation();
-
-            window.open(
-              `https://cfde-gene-pages.cloud/gene/${params.row.gene}?CF=false&PS=true&Ag=true&gene=false&variant=false`,
-              "_blank",
-              "noreferrer"
-            );
-          };
-
-          const onClickPrismEXP = (e) => {
-            e.stopPropagation();
-
-            window.open(
-              `https://maayanlab.cloud/prismexp/g/${params.row.gene}`,
-              "_blank",
-              "noreferrer"
-            );
-          };
-
-          return (
-            <div
-              className={styles.horizontalFlexbox}
-              style={{ gap: "0px", padding: "0px" }}
-            >
-              <Tooltip title="Open in Ensembl">
-                <Button onClick={onClickENSEMBL}>
-                  <img
-                    style={{
-                      width: "20px",
-                      display: "flex",
-                      flexDirection: "row",
-                      gap: "0px",
-                      padding: "0px",
-                    }}
-                    src={
-                      runtimeConfig.NEXT_PUBLIC_ENTRYPOINT +
-                      "/images/ensembl.png"
-                    }
-                    alt="archs4 Logo"
-                  />
-                </Button>
-              </Tooltip>
-              <Tooltip title="Open in ARCHS4">
-                <Button onClick={onClickARCHS4}>
-                  <img
-                    style={{
-                      width: "40px",
-                      display: "flex",
-                      flexDirection: "row",
-                      gap: "0px",
-                      padding: "0px",
-                    }}
-                    src={
-                      runtimeConfig.NEXT_PUBLIC_ENTRYPOINT +
-                      "/images/archs4.png"
-                    }
-                    alt="archs4 Logo"
-                  />
-                </Button>
-              </Tooltip>
-              <Tooltip title="Open in Harmonizome">
-                <Button onClick={onClickHARMONIZOME}>
-                  <img
-                    style={{
-                      width: "23px",
-                      display: "flex",
-                      flexDirection: "row",
-                      gap: "0px",
-                      padding: "0px",
-                      marginLeft: "0px",
-                    }}
-                    src={
-                      runtimeConfig.NEXT_PUBLIC_ENTRYPOINT +
-                      "/images/harmonizomelogo.png"
-                    }
-                    alt="archs4 Logo"
-                  />
-                </Button>
-              </Tooltip>
-              <Tooltip title="Open in PrismEXP">
-                <Button onClick={onClickPrismEXP}>
-                  <img
-                    sx={{ m: 1 }}
-                    style={{
-                      width: "23px",
-                      display: "flex",
-                      flexDirection: "row",
-                      gap: "0px",
-                      padding: "0px",
-                      marginLeft: "0px",
-                    }}
-                    src={
-                      runtimeConfig.NEXT_PUBLIC_ENTRYPOINT +
-                      "/images/PrismEXP.png"
-                    }
-                    alt="archs4 Logo"
-                  />
-                </Button>
-              </Tooltip>
-              <Tooltip title="Open in GDLPA">
-                <Button onClick={onClickGDLPA}>
-                  <img
-                    sx={{ m: 1 }}
-                    style={{
-                      width: "23px",
-                      display: "flex",
-                      flexDirection: "row",
-                      gap: "0px",
-                      padding: "0px",
-                      marginLeft: "0px",
-                    }}
-                    src={
-                      runtimeConfig.NEXT_PUBLIC_ENTRYPOINT + "/images/GDLPA.png"
-                    }
-                    alt="archs4 Logo"
-                  />
-                </Button>
-              </Tooltip>
-            </div>
-          );
-        },
-      },
+        minWidth: 130,
+      }
     ];
   } else {
     columns = [
